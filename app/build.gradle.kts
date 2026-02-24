f4b72e6f9f3b2888e0d055221257a035b1db6e22
Manthan Singla <manthansingla786@gmail.com>
2026-02-24 23:44:49 +0530

    Updated versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.yourpackage.sheshield"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.yourpackage.sheshield"
